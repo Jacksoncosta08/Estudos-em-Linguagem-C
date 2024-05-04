3cf77db9e52e13a2c4e7059369cc246dfa00fce7
Jackson Costa <127801612+Jacksoncosta08@users.noreply.github.com>
2024-05-04 19:24:09 -0300

    Inicio
    Primeiro código que estou subindo em linguagem, para não perder a tradição.

---
+++ Hello World!/Hello World.c
@@ -0,0 +1,17 @@
+#include <stdio.h> // Bibliotecas
+#include <stdlib.h>
+
+int main (){ //corpo principal do algoritmo
+	
+	printf("Hello, world!\n"); // comando para imprimir na tela uma mensagem.
+	
+	system("pause");
+	
+}
+
+// Anotações:
+// 1 - Incluir as bibliotecas necessarias para execução do código;
+// 2 - {} Tudo que estiver dentro das chaves será executado, e apenas o que estuver dentro;
+// 3 - \n = faz com que seja quebrada uma linha;
+// 4 - Antes de executar o programa devemos compilar;
+// 5 - Se for efetuada qualquer alteração no programa é necessário compilar novamente.
