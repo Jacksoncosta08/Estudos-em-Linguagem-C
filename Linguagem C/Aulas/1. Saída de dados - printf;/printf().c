44d2fb9eb8fe27482fea7116af75abf2a63072d2
Jackson Costa <127801612+Jacksoncosta08@users.noreply.github.com>
2024-05-05 22:19:43 -0300

    Atualização do texto

--- Linguagem C/Aulas/1. Saída de dados - printf;/printf().c
+++ Linguagem C/Aulas/1. Saída de dados - printf;/printf().c
@@ -2,32 +2,33 @@
 
 // printf("texto", <v1>, <v2>, ..., <vN>);
 
-// Especificadores de formato: printf()
+/*Especificadores de formato: printf()
 
-// %d - N�meros inteiros em base decimal
-// %x - Numeros inteiros em base hexadecimal
-// %f - Numeros em ponto flutuante (com casas decimais)
-// %e - N�meros em nota��o cient�fica (com casas decimais)
-// %c - Caracteres alfanum�ricos (texto)
-// %s - Sequencia de caracteres alfanum�ricos (texto)
-// %.<num> - Especifica quantos digitos ser�o impressos ap�s a v�rgula
+%d - Numeros inteiros em base decimal
+%x - Numeros inteiros em base hexadecimal
+%f - Numeros em ponto flutuante (com casas decimais)
+%e - Numeros em notacao cientifica (com casas decimais)
+%c - Caracteres alfanumericos (texto)
+%s - Sequencia de caracteres alfanumericos (texto)
+%.<num> - Especifica quantos digitos serao impressos apos a virgula
 
-// Sequ�ncias de escape: printf()
+// Sequencias de escape: printf()
 
-// (\a) Toca um bipe, alarme sonoro padr�o do sistema
-// (\b) Backspace
-// (\n) Quebra de linha
-// (\t) Tabula��o horizontal (TAB)
-// (\r) Retorna ao in�cio da linha
-// (\0) Caractere nulo
-// (\v) Tabula��o vertical
-// (\\) Caractere \
-// (\') Caractere '
-// (\") Caractere "
-// (\?) Caractere ?
-// (\123) Caractere relacionado ao c�digo 123 em octal (ASCII)
-// (\X12) Caractere relacionado ao c�digo 12 em hexadecimal (ASCII)
-// (\%) Caractere %
+(\a) Toca um bipe, alarme sonoro padr�o do sistema
+(\b) Backspace
+(\n) Quebra de linha
+(\t) Tabulacao horizontal (TAB)
+(\r) Retorna ao inicio da linha
+(\0) Caractere nulo
+(\v) Tabulacao vertical
+(\\) Caractere \
+(\') Caractere '
+(\") Caractere "
+(\?) Caractere ?
+(\123) Caractere relacionado ao codigo 123 em octal (ASCII)
+(\X12) Caractere relacionado ao codigo 12 em hexadecimal (ASCII)
+(\%) Caractere %
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
