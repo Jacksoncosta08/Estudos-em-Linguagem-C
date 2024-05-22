c9b615da904bc0f376ec50201541510e15ede1f7
Jackson Costa <127801612+Jacksoncosta08@users.noreply.github.com>
2024-05-22 18:46:50 -0300

    Update printf().c
    Retirada das anotações para outro arquivo.

--- Linguagem C/Aulas/1. Saída de dados - printf;/printf().c
+++ Linguagem C/Aulas/1. Saída de dados - printf;/printf().c
@@ -1,35 +1,3 @@
-/*Sintexe:
-
-printf("texto", <v1>, <v2>, ..., <vN>);
-
-Especificadores de formato: printf()
-
-%d - Numeros inteiros em base decimal
-%x - Numeros inteiros em base hexadecimal
-%f - Numeros em ponto flutuante (com casas decimais)
-%e - Numeros em notacao cientifica (com casas decimais)
-%c - Caracteres alfanumericos (texto)
-%s - Sequencia de caracteres alfanumericos (texto)
-%.<num> - Especifica quantos digitos serao impressos apos a virgula
-
-Sequencias de escape: printf()
-
-(\a) Toca um bipe, alarme sonoro padr�o do sistema
-(\b) Backspace
-(\n) Quebra de linha
-(\t) Tabulacao horizontal (TAB)
-(\r) Retorna ao inicio da linha
-(\0) Caractere nulo
-(\v) Tabulacao vertical
-(\\) Caractere \
-(\') Caractere '
-(\") Caractere "
-(\?) Caractere ?
-(\123) Caractere relacionado ao codigo 123 em octal (ASCII)
-(\X12) Caractere relacionado ao codigo 12 em hexadecimal (ASCII)
-(\%) Caractere %
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
